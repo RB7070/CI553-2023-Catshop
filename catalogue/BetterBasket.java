b1a699654002cd8cd3bbce777b09f4382a70c6e2
ryanm <ryanm@DESKTOP-BVFO7KD>
2024-01-06 18:52:06 +0000

    Added sort functionality

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -2,6 +2,7 @@ package catalogue;
 
 import java.io.Serializable;
 import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Write a description of class BetterBasket here.
@@ -9,10 +10,31 @@ import java.util.Collections;
  * @author  Your Name 
  * @version 1.0
  */
-public class BetterBasket extends Basket implements Serializable
+public class BetterBasket extends Basket implements Serializable, Comparator<Product>
 {
   private static final long serialVersionUID = 1L;
   
 
   // You need to add code here
+  
+  @Override
+  public boolean add(Product p1) {
+	  //search existing products for matching record
+	  for (Product p2: this) {
+		  if (p1.getProductNum().equals(p2.getProductNum())) {
+			  // found - update quantity and return
+			  p2.setQuantity(p2.getQuantity()+p1.getQuantity());
+			  return(true);
+		  }
+	  }
+	  // not found - add new product, using superclass method
+	  super.add(p1);
+	  
+	  Collections.sort(this, this);
+	  return(true);
+  }
+  
+  public int compare(Product p1, Product p2) {
+	  return p1.getProductNum().compareTo(p2.getProductNum());
+  }
 }
